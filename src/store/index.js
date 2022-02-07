f12aff9635da72d35c497a6b36311837d6017724
Jzone96 <jason.liberti@live.it>
2022-02-07 07:52:32 +0100

    firstCommit

---
+++ src/store/index.js
@@ -0,0 +1,19 @@
+import { createStore } from 'vuex';
+
+export default createStore({
+  state: {
+    authModalShow: false,
+  },
+  mutations: {
+    toggleAuthModal: (state) => {
+      state.authModalShow = !state.authModalShow;
+    },
+  },
+  actions: {
+  },
+  modules: {
+  },
+  getters: {
+    authModalShow: (state) => state.authModalShow,
+  },
+});
